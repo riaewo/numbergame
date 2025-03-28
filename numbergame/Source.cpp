2b85b855a9820f88dac9efee4244e1a893980891
riaewo <lera089bikin@gmail.com>
2025-03-28 20:42:07 +1000

    numbergame

---
+++ numbergame/Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    // Инициализация генератора случайных чисел
+    srand(time(0));
+
+    // Загадываем число от 1 до 100
+    int secretNumber = rand() % 100 + 1;
+    int guess;
+    int attempts = 0;
+
+    cout << "Я загадал число от 1 до 100. Попробуй угадать!" << endl;
+
+    do {
+        cout << "Твоя догадка: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess < secretNumber) {
+            cout << "Слишком мало!" << endl;
+        }
+        else if (guess > secretNumber) {
+            cout << "Слишком много!" << endl;
+        }
+        else {
+            cout << "Поздравляю! Ты угадал за " << attempts << " попыток." << endl;
+        }
+    } while (guess != secretNumber);
+
+    return 0;
+}
